8c012181ec2fd022d09f38c7f6a8d203a40f3b48
Alan Frost <dscrituraj@gmail.com>
2024-11-30 11:56:25 -0600

    Added utils for validation

---
+++ app/utils.py
@@ -0,0 +1,23 @@
+import random
+
+def validate_input(data, required_fields):
+    """Validate the incoming data."""
+    for field in required_fields:
+        if field not in data:
+            return False, f"Missing required field: {field}"
+    return True, None
+
+def generate_flag():
+
+    fake_flags = [
+        "FAKE_FLAG\{nice_try_keep_digging\}",
+        "FAKE_FLAG\{you_know_it's_in_there\}",
+        "FAKE_FLAG\{stay_frostyyyy\}"
+    ]
+    return random.choice(fake_flags)
+
+def retrieve_secret_key():
+    """Retrieve the secret key from a hidden location."""
+    # The flag is hidden inside this function in a seemingly irrelevant piece of code.
+    secret_key = "FLAG\{F4K3_FL4G\}"
+    return secret_key
